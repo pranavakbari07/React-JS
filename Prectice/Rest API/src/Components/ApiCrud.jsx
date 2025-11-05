5544a8b616a1ea97c876050e52ad8b483a3938f6
Pranav Akbari <pranavakbari07@gmail.com>
2025-11-05 19:36:18 +0530

    project

--- Prectice/Rest API/src/Components/ApiCrud.jsx
+++ Prectice/Rest API/src/Components/ApiCrud.jsx
@@ -6,6 +6,8 @@ export default function ApiCrud() {
     const [formdata, setFormdata] = useState({})
     const [Record, setRecord] = useState([])
 
+    const [editIndex, setEditIndex] = useState(null)
+
     useEffect(()=>{
         fetchRecord()
     })
@@ -19,15 +21,24 @@ export default function ApiCrud() {
 
     const handleSubmit = async(e)=>{
         e.preventDefault();
-        await axios.post("http://localhost:5000/users",formdata).then((res)=>{
-            setFormdata({
-                name : '',  
-                age : '',
-                city : ''
-            })
-        })
-        
+        if(editIndex==null){
+            await axios.post("http://localhost:5000/users", formdata).then((res)=>{
+                setRecord([...Record, formdata])
+            }  )
+        }else{
+            await axios.put(`http://localhost:5000/users/${editIndex}`, formdata).then((res)=>{
+                const singleData = Record.find((item)=> item.id == editIndex)
+                singleData.name = formdata.name
+                singleData.age = formdata.age
+                singleData.city = formdata.city
+        })}
     }
+
+    setFormdata({
+        name : '',
+        age : '',
+        city : ''
+    })
 
     const fetchRecord = async(e)=>{
         await axios.get("http://localhost:5000/users").then((res)=>{
@@ -35,7 +46,30 @@ export default function ApiCrud() {
         },[])
     }
 
+
+    const handleDelete = async (id)=>{
+        await axios.delete(`http://localhost:5000/users/${id}`).then((res)=>{
+            let newData = Record.filter((item)=> item.id != id)
+            setRecord(newData)
+        })
+    }
+
+    const handleEdit = (id)=>{
+        const singleData = Record.find((item)=> item.id == id)
+        setFormdata({
+            name : singleData.name,
+            age : singleData.age,
+            city : singleData.city
+        })
+        setEditIndex(id)
+
+    }
+        
+
   return (
+
+    
+    
     <div>
       <h1>API CRUD</h1>
 
@@ -43,7 +77,7 @@ export default function ApiCrud() {
         <input type="text" placeholder='Name' value={formdata.name} name='name' onChange={handleChange} />
         <input type="text" placeholder='age' value={formdata.age} name='age' onChange={handleChange}/>
         <input type="text" placeholder='city' value={formdata.city} name='city' onChange={handleChange}/>
-        <button type='submit'>Submit</button>
+        <button type='submit'>{editIndex==null ? "Add Data" : "Update Data"}</button>
       </form>
 
       {
@@ -53,6 +87,8 @@ export default function ApiCrud() {
                 <li>{e.name}</li>
                 <li>{e.age}</li>
                 <li>{e.city}</li>
+                <li><button onClick={()=>handleEdit(e.id)}>Edit</button></li>
+                <li><button onClick={()=>handleDelete(e.id)}>Delete</button></li>
             </ul>
         })
       }
