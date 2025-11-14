a06d1ff1b753afe39f814a25569c50cb7bc54607
Pranav Akbari <pranavakbari07@gmail.com>
2025-11-14 11:34:38 +0530

    project

--- Project/Redux CRUD Project/src/app/Store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import api from "../features/ApiSlice.jsx";
-
-export const store = configureStore({
-    reducer : {
-        ApiKey : api
-    }
-})
